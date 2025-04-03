71b6f20ea0d319396bea556de9c5a6f9d8447d8e
Vincent Fowler <vincentowler@outlook.com>
2025-04-03 00:37:43 -0500

    Fix team members data handling and enhance organization member retrieval
    - Updated the team members state setting to handle cases where no members are returned, ensuring an empty array is set instead of undefined.
    - Improved the organization members retrieval logic to handle both numeric and public ID formats for organization IDs, enhancing data accuracy.
    - Adjusted the API response to filter and map user data correctly, ensuring only valid user entries are returned.

--- apps/web/src/app/api/v1/organization/members/route.ts
+++ apps/web/src/app/api/v1/organization/members/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const [, authUser] = await user(req);
+    console.log("🚀 ~ GET ~ authUser:", authUser)
 
     if (!authUser) {
       console.error("Session does not exist.");
@@ -13,14 +14,30 @@ export async function GET(req: NextRequest) {
         { status: 500 }
       );
     }
+    let organizationId = authUser.user_metadata.organizationId;
+    if(isNaN(Number(authUser.user_metadata.organizationId))) {
+      const { data: organization } = await supabaseServiceRole
+        .from("Organization")
+        .select("id")
+        .eq("publicId", authUser.user_metadata.organizationId)
+        .single();
+      organizationId = organization?.id;
+    }
 
     const data = await supabaseServiceRole
-      .from("User")
-      .select("*")
-      .eq("organizationId", authUser.user_metadata.organizationId)
-      .neq("isSupportUser", true);
+      .from("UserToOrganization")
+      .select("*,User(*)")
+      .eq("organizationId", organizationId)
+      .eq("isDeleted", false)
+      .neq("User.isDeleted", true)
+      .neq("User.isSupportUser", true);
+      // .neq("isSupportUser", true);
+      console.log("🚀 ~ GET ~ data:", data)
 
-    return NextResponse.json({ status: "ok", members: data.data });
+    return NextResponse.json({ status: "ok", members: data.data?.filter((member)=>member.User).map((member)=>({
+      ...member,
+      ...member.User
+    })) || [] });
   } catch {
     return NextResponse.json({ status: "failed" }, { status: 500 });
   }
