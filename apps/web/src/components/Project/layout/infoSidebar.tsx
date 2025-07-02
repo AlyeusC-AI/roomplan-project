33a319ddf7116785452513447757db80f5924494
Thomas Ray <tms.ray@outlook.com>
2025-07-02 16:41:46 -0500

    add project sider collapsible

--- apps/web/src/components/Project/layout/infoSidebar.tsx
+++ apps/web/src/components/Project/layout/infoSidebar.tsx
@@ -12,13 +12,11 @@ import ProjectUsersCard from "./ProjectUsersCard";
 import DescriptionCard from "./DescriptionCard";
 import TasksCard from "./TasksCard";
 import ProjectConversationCard from "./ProjectConversationCard";
-import { useState } from "react";
 import clsx from "clsx";
 
-export default function InfoSidebar() {
+export default function InfoSidebar({ isCollapsed, setIsCollapsed }: { isCollapsed: boolean, setIsCollapsed: (isCollapsed: boolean) => void }) {
   const { id } = useParams();
   const pathname = usePathname();
-  const [isCollapsed, setIsCollapsed] = useState(false);
   const { data: project } = useGetProjectById(id as string);
   const projectData = project?.data;
   if (!projectData) return null;
@@ -27,51 +25,31 @@ export default function InfoSidebar() {
   return (
     <div
       className={clsx(
-        "relative",
+        "",
         isCollapsed ? "w-5 min-w-5" : "w-full w-[364px]"
       )}
     >
       <div
         className={clsx(
           "fixed right-0 top-0 z-30 bg-accent h-screen overflow-y-auto",
-          isCollapsed ? "w-20 min-w-20" : "w-[200px] lg:w-[240px] xl:w-[280px] 2xl:w-[364px]"
+          isCollapsed ? "w-12 min-w-12" : "w-[200px] lg:w-[240px] xl:w-[280px] 2xl:w-[364px]"
         )}
       >
-        {isCollapsed ? (
-          <button
-            className='absolute -left-6 top-4 flex h-10 w-10 items-center justify-center rounded-lg border-0 bg-accent'
-            onClick={() => setIsCollapsed(false)}
-            aria-label='Expand sidebar'
-            style={{ zIndex: 40 }}
-          >
-            <ChevronLeft size={24} />
-          </button>
-        ) : (
-          <>
-            {/* <button
-              className='absolute -left-6 top-4 flex h-10 w-10 items-center justify-center rounded-lg border-0 bg-accent'
-              onClick={() => setIsCollapsed(true)}
-              aria-label='Collapse sidebar'
-              style={{ zIndex: 40 }}
-            >
-              <ChevronRight size={24} />
-            </button> */}
-            <div className='space-y-4 p-4'>
-              <div className='space-y-4 divide-y divide-gray-200 rounded-lg border border-border bg-background p-4'>
-                <ContactInfoCard projectData={projectData} />
-                <ProjectUsersCard projectData={projectData} />
-                <AdjusterInfoCard projectData={projectData} />
-                <DescriptionCard projectData={projectData} />
-              </div>
-              <div className='rounded-lg border border-border bg-background p-4'>
-                <TasksCard />
-              </div>
-              <div className='rounded-lg border border-border bg-background p-4'>
-                <ProjectConversationCard projectId={projectData.id} />
-              </div>
-            </div>
-          </>
-        )}
+        {!isCollapsed && (<div className='space-y-4 p-4'>
+          <div className='space-y-4 divide-y divide-gray-200 rounded-lg border border-border bg-background p-4'>
+            <ContactInfoCard projectData={projectData} />
+            <ProjectUsersCard projectData={projectData} />
+            <AdjusterInfoCard projectData={projectData} />
+            <DescriptionCard projectData={projectData} />
+          </div>
+          <div className='rounded-lg border border-border bg-background p-4'>
+            <TasksCard />
+          </div>
+          <div className='rounded-lg border border-border bg-background p-4'>
+            <ProjectConversationCard projectId={projectData.id} />
+          </div>
+        </div>)
+        }
       </div>
     </div>
   );
